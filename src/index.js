107355c0dc94fc6e486c12b95443bb0fa2bb5d4b
Yiou Chen <yiou.chen@veeva.com>
2018-09-29 15:58:14 -0700

    change source when button click

--- src/index.js
+++ src/index.js
@@ -1,53 +1,56 @@
 import { SourceMapGenerator } from "source-map";
 import convert from "convert-source-map";
 
-const map = new SourceMapGenerator({
-  file: "dist.js"
-});
+function generateSourceMap(content, dist, mapping) {
+    const map = new SourceMapGenerator({
+        file: "dist.js",
+    });
+
+    map.setSourceContent("Original.js", content);
+    map.addMapping(mapping);
+    return convert
+        .fromJSON(map.toString())
+        .toComment()
+        .replace("//#", "/*#") + " */";
+}
 
 const content = `// This line will be skipped
 I will set css class here
 I will set color here
 `;
-map.setSourceContent("Original.js", content);
+
 
 const generated = `div {
   color: red;  
 }
 `;
-map.addMapping({
-  generated: {
-    line: 1,
-    column: 0
-  },
-  source: "Original.js",
-  original: {
-    line: 2,
-    column: 0
-  }
-});
 
-map.addMapping({
-  generated: {
-    line: 2,
-    column: 1
-  },
-  source: "Original.js",
-  original: {
-    line: 3,
-    column: 0
-  }
-});
+const firstMapping = {
+    generated: {
+        line: 1,
+        column: 0
+    },
+    source: "Original.js",
+    original: {
+        line: 2,
+        column: 0
+    }
+};
 
+const sourceMapComment = generateSourceMap(content, generated, firstMapping);
 const style = document.createElement("style");
-
 style.type = "text/css";
-
-const sourceMapComment =
-  convert
-    .fromJSON(map.toString())
-    .toComment()
-    .replace("//#", "/*#") + " */";
 style.appendChild(document.createTextNode(generated + sourceMapComment));
 document.head.appendChild(style);
-console.log(sourceMapComment);
+
+const changedContent = `// I am a totally different file
+I like piza
+I also like coffee
+`;
+
+const button = document.querySelector('button');
+
+button.addEventListener('click', () => {
+    const changedSourceMap = generateSourceMap(changedContent, generated, firstMapping);
+    style.innerHTML = generated + changedSourceMap;
+});
